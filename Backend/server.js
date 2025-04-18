42bc60cbb6c52ab8c0725ed7f209bcdc08f91882
manda <manda.li8686@gmail.com>
2025-04-18 17:46:45 -0500

    reupload models

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,8 @@
-console.log('starting');
+const express = require('express');
+
+const app = express();
+const port = 5400;
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
