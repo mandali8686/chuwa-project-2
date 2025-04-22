eda188a5f6bb436e8b21791db2dfe20aa1001ed0
阮书浩 <14543029+shuhaoruan@user.noreply.gitee.com>
2025-04-21 22:31:45 -0400

    Complete HR module: login, token, register, list, review

--- Backend/mongoConnect.js
+++ Backend/mongoConnect.js
@@ -1,7 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://training:shangan@fullstack-training.gw3nkbl.mongodb.net/demoDec?retryWrites=true&w=majority')
-.then(()=>{console.log('Connected to MongoDB')})
-.catch((e)=>{console.log('Error connecting to MongoDB', e)});
+mongoose
+  .connect(
+    "mongodb+srv://training:shangan@fullstack-training.gw3nkbl.mongodb.net/demoDec?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((e) => {
+    console.log("Error connecting to MongoDB", e);
+  });
 
 module.exports = mongoose;
