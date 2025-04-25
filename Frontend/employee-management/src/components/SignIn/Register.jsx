b1801bc2d9488451940e2a87f18ed6a5ca28a541
manda <manda.li8686@gmail.com>
2025-04-25 13:19:15 -0500

    sign in and register working

--- Frontend/employee-management/src/components/SignIn/Register.jsx
+++ Frontend/employee-management/src/components/SignIn/Register.jsx
@@ -1,43 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Form, Input, Button, Typography, Card, message } from 'antd';
-import styled from '@emotion/styled';
-import { createUserAsync } from '../../features/employee/index';
+import { Form, Input, Button, Typography, message } from 'antd';
+import { updateUser } from '../../features/employee/index';
 import { useParams } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import { CardContainer, AuthContainer, StyledForm, SectionTitle, InfoCard } from '../../assets/SignInComponents';
 
-const { Title } = Typography;
-
-const PageContainer = styled.div`
-  max-width: 500px;
-  margin: 0 auto;
-  padding: 24px;
-`;
-
-const SectionTitle = styled(Title)`
-  && {
-    color: #8b5e3c;
-    margin-bottom: 16px;
-  }
-`;
-
-const InfoCard = styled(Card)`
-  margin-bottom: 24px;
-  border-radius: 10px;
-`;
+// const { Title } = Typography;
 
 const Register = () => {
   const dispatch = useDispatch();
   const { token } = useParams();
   const [form] = Form.useForm();
+  const decoded = jwtDecode(token);
+  const { employeeId, email } = decoded;
+
+  useEffect(() => {
+    form.setFieldsValue({ email });  
+  }, [email, form]);
 
   const onFinish = (values) => {
     const payload = {
-      username: values.username,
-      email: values.email,
-      password: values.password,
+      userId: employeeId,                          
+      updatedData: {                               
+        username: values.username,
+        email: values.email,
+        password: values.password,
+      },
     };
 
-    dispatch(createUserAsync(payload))
+    dispatch(updateUser(payload))
       .unwrap()
       .then(() => {
         message.success('Registration successful!');
@@ -49,28 +41,35 @@ const Register = () => {
   };
 
   return (
-    <PageContainer>
-      <SectionTitle level={2}>User Registration</SectionTitle>
-      <Form layout="vertical" onFinish={onFinish} form={form}>
-        <InfoCard title="Account Information">
-          <Form.Item name="username" label="Username" rules={[{ required: true }]}>
-            <Input placeholder="Enter username" />
-          </Form.Item>
-          <Form.Item name="email" label="Email" rules={[{ required: true, type: 'email' }]}>
-            <Input placeholder="Enter email" />
-          </Form.Item>
-          <Form.Item name="password" label="Password" rules={[{ required: true }]}>
-            <Input.Password placeholder="Enter password" />
-          </Form.Item>
-        </InfoCard>
+    <CardContainer>
+      <AuthContainer>
+        <SectionTitle level={2}>User Registration</SectionTitle>
+        <StyledForm
+          layout="vertical"
+          onFinish={onFinish}
+          form={form}
+          initialValues={{ email }}  
+        >
+          <InfoCard title="Account Information">
+            <Form.Item name="username" label="Username" rules={[{ required: true }]}>
+              <Input placeholder="Enter username" />
+            </Form.Item>
+            <Form.Item name="email" label="Email" rules={[{ required: true, type: 'email' }]}>
+              <Input placeholder="Enter email" />
+            </Form.Item>
+            <Form.Item name="password" label="Password" rules={[{ required: true }]}>
+              <Input.Password placeholder="Enter password" />
+            </Form.Item>
+          </InfoCard>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" block>
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
-    </PageContainer>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" block>
+              Register
+            </Button>
+          </Form.Item>
+        </StyledForm>
+      </AuthContainer>
+    </CardContainer>
   );
 };
 
