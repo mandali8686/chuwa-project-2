455d28ee9bceca2676c3d0b1404e6e3b5514c24e
manda <manda.li8686@gmail.com>
2025-04-25 01:28:51 -0500

    fixed reducers in store

--- Frontend/employee-management/src/redux/store.js
+++ Frontend/employee-management/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import hrReducer from "../features/hr/hrSlice";
+import { hrReducer }from "../features/hr/hrSlice";
+import { userReducer } from "../features/employee/index";
 
 const store = configureStore({
   reducer: {
-    hr: hrReducer,
+    user: userReducer,
+    hr: hrReducer
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: true,
